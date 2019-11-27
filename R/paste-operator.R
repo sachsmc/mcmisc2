ab1c5e35a79c1b0d8fb9305555ff022099035ba7
Michael Sachs <micsac@kep.local>
2019-11-27 15:32:41 +0100

    Documented the paste operator

--- R/paste-operator.R
+++ R/paste-operator.R
@@ -1,4 +1,12 @@
-
+#' Paste operator
+#'
+#' An infix operator to combine strings.
+#'
+#' @param a String 1
+#' @param b string 2
+#'
+#' @return a string
+#' @export
 
 `% %` <- function(a, b) {
 
